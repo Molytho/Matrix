1b979622067a5c11c0fe04cb6a6397ce27be2eb5
Robin Ebert <ebertrobin2002@gmail.com>
2020-10-09 17:02:46 +0200

    Added Matrix type with basic storage ability

---
+++ Matrix/Dimension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Molytho.Matrix
+{
+    public readonly struct Dimension
+    {
+        public Dimension(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public readonly int Height { get; }
+        public readonly int Width { get; }
+    }
+}
---
+++ Matrix/Matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Molytho.Matrix
+{
+    public class Matrix<T>
+    {
+        private readonly Dimension _dimension;
+        private readonly T[,] _data;
+
+        public Matrix(int height, int width)
+        {
+            _dimension = new Dimension(height, width);
+            _data = new T[height, width];
+        }
+
+        public Dimension Dimension => _dimension;
+        public int Height => _dimension.Height;
+        public int Width => _dimension.Width;
+
+        public ref T this[int row, int column]
+            => ref _data[row,column];
+    }
+}
