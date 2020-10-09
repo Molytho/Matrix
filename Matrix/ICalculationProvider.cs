43938b2c10e9cf20c36c2b67068bef10a91b9c42
Robin Ebert <ebertrobin2002@gmail.com>
2020-10-09 21:29:09 +0200

    Add methods to calculate on an already existing instance

--- Matrix/ICalculationProvider.cs
+++ Matrix/ICalculationProvider.cs
@@ -11,7 +11,12 @@ namespace Molytho.Matrix
         public Matrix<T> Substract(Matrix<T> a, Matrix<T> b);
         public Matrix<T> Multipy(Matrix<T> a, Matrix<T> b);
         public Matrix<T> Multipy(Matrix<T> a, T b);
-        public Matrix<T> Multipy(T a, Matrix<T> b);
+
+        public void InverseThis(ref Matrix<T> ret, Matrix<T> a);
+        public void AddThis(ref Matrix<T> ret, Matrix<T> a, Matrix<T> b);
+        public void SubstractThis(ref Matrix<T> ret, Matrix<T> a, Matrix<T> b);
+        public void MultipyThis(ref Matrix<T> ret, Matrix<T> a, Matrix<T> b);
+        public void MultipyThis(ref Matrix<T> ret, Matrix<T> a, T b);
     }
     public static class CalculationProvider<T>
     {
@@ -68,7 +73,6 @@ namespace Molytho.Matrix
             {
                 throw new NotImplementedException();
             }
-
             public Matrix<int> Multipy(Matrix<int> a, Matrix<int> b)
             {
                 if (a.Width != b.Height)
@@ -89,8 +93,6 @@ namespace Molytho.Matrix
 
                 return ret;
             }
-
-            public Matrix<int> Multipy(int a, Matrix<int> b) => Multipy(b, a);
             public Matrix<int> Multipy(Matrix<int> a, int b)
             {
                 Matrix<int> ret = new Matrix<int>(a.Dimension);
@@ -101,8 +103,6 @@ namespace Molytho.Matrix
                     }
                 return ret;
             }
-
-
             public Matrix<int> Substract(Matrix<int> a, Matrix<int> b)
             {
                 if (!a.Dimension.Equals(b.Dimension))
@@ -115,6 +115,57 @@ namespace Molytho.Matrix
                         ret[x,y] = a[x, y] - b[x, y];
                     }
                 return ret;
+            }
+
+            public void InverseThis(ref Matrix<int> ret, Matrix<int> a)
+            {
+                throw new NotImplementedException();
+            }
+            public void MultipyThis(ref Matrix<int> ret, Matrix<int> a, Matrix<int> b)
+            {
+                if (a.Width != b.Height || !(ret.Height == a.Height && ret.Width == b.Width))
+                    throw new DimensionMismatchException();
+
+                for (int x = 0; x < b.Width; x++)
+                    for (int y = 0; y < a.Height; y++)
+                    {
+                        int value = 0;
+                        for (int i = 0; i < a.Width; i++)
+                        {
+                            value += a[i, y] * b[x, i];
+                        }
+                        ret[x, y] = value;
+                    }
+            }
+            public void MultipyThis(ref Matrix<int> ret, Matrix<int> a, int b)
+            {
+                for (int x = 0; x < a.Width; x++)
+                    for (int y = 0; y < a.Height; y++)
+                    {
+                        ret[x, y] = a[x, y] * b;
+                    }
+            }
+            public void AddThis(ref Matrix<int> ret, Matrix<int> a, Matrix<int> b)
+            {
+                if (!a.Dimension.Equals(b.Dimension) || !a.Dimension.Equals(ret))
+                    throw new DimensionMismatchException();
+
+                for (int x = 0; x < a.Width; x++)
+                    for (int y = 0; y < a.Height; y++)
+                    {
+                        ret[x, y] = a[x, y] + b[x, y];
+                    }
+            }
+            public void SubstractThis(ref Matrix<int> ret, Matrix<int> a, Matrix<int> b)
+            {
+                if (!a.Dimension.Equals(b.Dimension) || !a.Dimension.Equals(ret))
+                    throw new DimensionMismatchException();
+
+                for (int x = 0; x < a.Width; x++)
+                    for (int y = 0; y < a.Height; y++)
+                    {
+                        ret[x, y] = a[x, y] - b[x, y];
+                    }
             }
         }
     }
