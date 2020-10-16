75ed6394e8cfbd86f400fdd0ad41d54a500d8aac
Robin Ebert <ebertrobin2002@gmail.com>
2020-10-16 17:58:09 +0200

    Added FloatCalculationProvider TransposeAvx2 implementation

--- Matrix/CalculationProvider/CalculationProviders/FloatCalculationProvider.cs
+++ Matrix/CalculationProvider/CalculationProviders/FloatCalculationProvider.cs
@@ -68,17 +68,85 @@ namespace Molytho.Matrix.Calculation.Providers
 
             return ret;
         }
-        public MatrixBase<float> Transpose(MatrixBase<float> a)
+
+        private unsafe void TransposeAvx2(MatrixBase<float> ret, MatrixBase<float> a)
+        {
+            fixed (float* base_a = &a[0, 0], base_ret = &ret[0, 0])
+            {
+                int calculated = 0;
+                int* indices = stackalloc int[]
+                {
+                    0,
+                    a.Width * 1 * sizeof(float),
+                    a.Width * 2 * sizeof(float),
+                    a.Width * 3 * sizeof(float),
+                    a.Width * 4 * sizeof(float),
+                    a.Width * 5 * sizeof(float),
+                    a.Width * 6 * sizeof(float),
+                    a.Width * 7 * sizeof(float)
+                };
+                Vector256<int> indexVec = Avx.LoadVector256(indices);
+                if (a.Height >= Vector256<float>.Count)
+                {
+                    int indexScale = a.Width * 8 * sizeof(float);
+                    Vector256<int> indexVecInc = Avx2.BroadcastScalarToVector256(&indexScale);
+                    while (calculated + Vector256<float>.Count < a.Height)
+                    {
+                        if (calculated > 0)
+                            indexVec = Avx2.Add(indexVec, indexVecInc);
+
+                        for (int x = 0; x < a.Width; x++)
+                        {
+                            Vector256<float> storeVec = Avx2.GatherVector256(base_a + x, indexVec, 1);
+                            Avx.Store(base_ret + x * ret.Width + calculated, storeVec);
+                        }
+                        calculated += Vector256<float>.Count;
+                    }
+                    if (a.Height % Vector256<float>.Count > 1)
+                        indexVec = Avx2.Add(indexVec, indexVecInc);
+                }
+                if (a.Height % Vector256<float>.Count > 1)
+                {
+                    float* masks = stackalloc float[Vector256<float>.Count];
+                    for (int i = 0; i < Vector256<float>.Count; i++)
+                        //Most significant bit is used as mask
+                        masks[i] = a.Height - calculated - i > 0 ? (float)(0x80000000) : 0;
+
+                    Vector256<float> maskVec = Avx.LoadVector256(masks);
+                    for (int x = 0; x < a.Width; x++)
+                    {
+                        Vector256<float> storeVec = Avx2.GatherMaskVector256(Vector256<float>.Zero, base_a + x, indexVec, maskVec, 1);
+                        Avx2.MaskStore(base_ret + x * ret.Width + calculated, maskVec, storeVec);
+                    }
+                }
+                else if (a.Height % Vector256<float>.Count == 1)
+                {
+                    for (int x = 0; x < a.Width; x++)
+                    {
+                        *(base_ret + x * ret.Width + calculated) = *(base_a + x + calculated * a.Width);
+                    }
+                }
+            }
+        }
+        public unsafe MatrixBase<float> Transpose(MatrixBase<float> a)
         {
             MatrixBase<float> ret =
                 a.Height == 1
                 ? new Vector<float>(a.Width) as MatrixBase<float>
                 : new Matrix<float>(a.Width, a.Height) as MatrixBase<float>;
-            for (int y = 0; y < a.Height; y++)
-                for (int x = 0; x < a.Width; x++)
-                {
-                    ret[y, x] = a[x, y];
-                }
+            if (a.Height == 1 || a.Width == 1)
+            {
+                fixed (float* base_a = &a[0, 0], base_ret = &ret[0, 0])
+                    MemoryOperation.memcpy(base_ret, base_a, a.Height * a.Width * sizeof(float));
+            }
+            else if (Avx2.IsSupported)
+                TransposeAvx2(ret, a);
+            else
+                for (int y = 0; y < a.Height; y++)
+                    for (int x = 0; x < a.Width; x++)
+                    {
+                        ret[y, x] = a[x, y];
+                    }
             return ret;
         }
 
