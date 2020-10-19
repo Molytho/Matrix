6d66fd2005b2fe26a7131171f04bff3136fe0663
Robin Ebert <ebertrobin2002@gmail.com>
2020-10-19 12:04:09 +0200

    Added Vector class

---
+++ Matrix/Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Molytho.Matrix
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class Vector<T> : MatrixBase<T>
+    {
+        private readonly T[] _data;
+
+        public Vector(int size) : base(size, 1)
+        {
+            _data = new T[size];
+        }
+        public Vector(Dimension dimension) : base(dimension)
+        {
+            if (dimension.Width != 1)
+                throw new DimensionMismatchException();
+
+            _data = new T[dimension.Height];
+        }
+
+        public override ref T this[int x, int y]
+        {
+            get
+            {
+                if (x != 0)
+                    throw new IndexOutOfRangeException();
+
+                return ref _data[y];
+            }
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
