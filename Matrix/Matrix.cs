9a67a700f07a0408965e2313749c7ceb1031141f
Robin Ebert <ebertrobin2002@gmail.com>
2020-10-19 11:57:45 +0200

    Move functionality to MatrixBase

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -7,61 +7,25 @@ namespace Molytho.Matrix
     [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
     public class Matrix<T> : MatrixBase<T>
     {
-        private readonly Dimension _dimension;
         private readonly T[,] _data;
 
-        public Matrix(int height, int width)
+        public Matrix(int height, int width) : base(height, width)
         {
-            _dimension = new Dimension(height, width);
             _data = new T[height, width];
         }
-        public Matrix(Dimension dimension)
+        public Matrix(Dimension dimension) : base(dimension)
         {
-            _dimension = dimension;
             _data = new T[dimension.Height, dimension.Width];
         }
 
-        public override Dimension Dimension => _dimension;
-        public override int Height => _dimension.Height;
-        public override int Width => _dimension.Width;
-
         public override ref T this[int x, int y]
             => ref _data[y, x];
-
-        public override MatrixBase<T> Transpose => CalculationProvider<T>.Provider.Transpose(this);
 
         public static MatrixBase<T> operator +(Matrix<T> a, Matrix<T> b) => CalculationProvider<T>.Provider.Add(a, b);
         public static MatrixBase<T> operator -(Matrix<T> a, Matrix<T> b) => CalculationProvider<T>.Provider.Substract(a, b);
         public static MatrixBase<T> operator *(Matrix<T> a, Matrix<T> b) => CalculationProvider<T>.Provider.Multipy(a, b);
         public static MatrixBase<T> operator *(Matrix<T> a, T b) => CalculationProvider<T>.Provider.Multipy(a, b);
         public static MatrixBase<T> operator *(T a, Matrix<T> b) => CalculationProvider<T>.Provider.Multipy(b, a);
-
-        public override void Inverse() => CalculationProvider<T>.Provider.InverseThis(this, this);
-        public override void Add(MatrixBase<T> other) => CalculationProvider<T>.Provider.AddThis(this, this, other);
-        public override void Substract(MatrixBase<T> other) => CalculationProvider<T>.Provider.SubstractThis(this, this, other);
-        public override void Multipy(MatrixBase<T> other) => CalculationProvider<T>.Provider.MultipyThis(this, this, other);
-        public override void Multipy(T scalar) => CalculationProvider<T>.Provider.MultipyThis(this, this, scalar);
-
-        public override string ToString()
-        {
-            StringBuilder builder = new StringBuilder();
-            builder.Append('{');
-            for (int y = 0; y < Height; y++)
-            {
-                builder.Append('{');
-                for (int x = 0; x < Width; x++)
-                {
-                    builder.Append(this[x, y]);
-                    if (x < Width - 1)
-                        builder.Append(", ");
-                }
-                builder.Append('}');
-                if (y < Height - 1)
-                    builder.Append(", ");
-            }
-            builder.Append('}');
-            return builder.ToString();
-        }
 
         private string GetDebuggerDisplay()
         {
--- Matrix/MatrixBase.cs
+++ Matrix/MatrixBase.cs
@@ -1,18 +1,53 @@
-﻿namespace Molytho.Matrix
+﻿using System.Text;
+using Molytho.Matrix.Calculation;
+
+namespace Molytho.Matrix
 {
     public abstract class MatrixBase<T>
     {
+        public MatrixBase(int height, int width)
+        {
+            _dimension = new Dimension(height, width);
+        }
+        public MatrixBase(Dimension dimension)
+        {
+            _dimension = dimension;
+        }
+
         public abstract ref T this[int x, int y] { get; }
 
-        public abstract Dimension Dimension { get; }
-        public abstract int Height { get; }
-        public abstract MatrixBase<T> Transpose { get; }
-        public abstract int Width { get; }
+        private readonly Dimension _dimension;
+        public Dimension Dimension => _dimension;
+        public int Height => Dimension.Height;
+        public int Width => Dimension.Width;
 
-        public abstract void Add(MatrixBase<T> other);
-        public abstract void Inverse();
-        public abstract void Multipy(MatrixBase<T> other);
-        public abstract void Multipy(T scalar);
-        public abstract void Substract(MatrixBase<T> other);
+        public virtual MatrixBase<T> Transpose => CalculationProvider<T>.Provider.Transpose(this);
+
+        public virtual void Inverse() => CalculationProvider<T>.Provider.InverseThis(this, this);
+        public virtual void Add(MatrixBase<T> other) => CalculationProvider<T>.Provider.AddThis(this, this, other);
+        public virtual void Substract(MatrixBase<T> other) => CalculationProvider<T>.Provider.SubstractThis(this, this, other);
+        public virtual void Multipy(MatrixBase<T> other) => CalculationProvider<T>.Provider.MultipyThis(this, this, other);
+        public virtual void Multipy(T scalar) => CalculationProvider<T>.Provider.MultipyThis(this, this, scalar);
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append('{');
+            for (int y = 0; y < Height; y++)
+            {
+                builder.Append('{');
+                for (int x = 0; x < Width; x++)
+                {
+                    builder.Append(this[x, y]);
+                    if (x < Width - 1)
+                        builder.Append(", ");
+                }
+                builder.Append('}');
+                if (y < Height - 1)
+                    builder.Append(", ");
+            }
+            builder.Append('}');
+            return builder.ToString();
+        }
     }
 }
